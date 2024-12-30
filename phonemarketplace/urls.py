23425f972028238672d7ae0d63d7bf1b4a86dc66
Fahad <rajafahad03425@gmail.com>
2024-12-30 11:11:04 +0500

    new new

--- phonemarketplace/urls.py
+++ phonemarketplace/urls.py
@@ -32,4 +32,4 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
    
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
